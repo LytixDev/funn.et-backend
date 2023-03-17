3ce016a2889f7bf38d645ad09bc38542b5a46485
Carl Gützkow <cjgutzkow@gmail.com>
2023-03-17 14:02:18 +0100

    add javadoc to spring security config class

--- src/main/java/edu/ntnu/idatt2105/placeholder/security/SecurityConfig.java
+++ src/main/java/edu/ntnu/idatt2105/placeholder/security/SecurityConfig.java
@@ -6,10 +6,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * Configures spring security
+ * 
+ * @author Carl G.
+ * @version 1.0
+ * @date 17.3.2023
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    /**
+     * Permits all requests to `swagger-ui/index.html` and `v3/api-docs`.
+     * 
+     * @param http HttpSecurity - http object to build configurations on
+     * @throws Exception thrown if an error occurs when permitting requests
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.securityMatcher("/swagger-ui/index.html", "v3/api-docs").csrf().disable()
