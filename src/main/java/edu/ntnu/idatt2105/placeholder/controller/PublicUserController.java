02fa9a2a944cc9fef3537bb5310dfd7ed315ae9c
Carl Gützkow <cjgutzkow@gmail.com>
2023-03-20 19:22:10 +0100

    Create register mapper and not blank on register dto

--- src/main/java/edu/ntnu/idatt2105/placeholder/controller/PublicUserController.java
+++ src/main/java/edu/ntnu/idatt2105/placeholder/controller/PublicUserController.java
@@ -1,10 +1,14 @@
 package edu.ntnu.idatt2105.placeholder.controller;
 
+import edu.ntnu.idatt2105.placeholder.dto.user.RegisterDTO;
 import edu.ntnu.idatt2105.placeholder.dto.user.UserDTO;
 import edu.ntnu.idatt2105.placeholder.exceptions.user.UserDoesNotExistsException;
+import edu.ntnu.idatt2105.placeholder.mapper.user.RegisterMapper;
 import edu.ntnu.idatt2105.placeholder.mapper.user.UserMapper;
 import edu.ntnu.idatt2105.placeholder.model.user.User;
 import edu.ntnu.idatt2105.placeholder.service.user.UserService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -46,8 +50,9 @@ public class PublicUserController {
     }
   }
 
-  @PostMapping("")
-  public ResponseEntity<String> createUser(User user) {
+  @PostMapping
+  public ResponseEntity<String> createUser(@RequestBody RegisterDTO registerUser) {
+    User user = RegisterMapper.INSTANCE.registerDTOtoUser(registerUser);
     try {
       userService.saveUser(user);
     } catch (Exception e) {
--- src/main/java/edu/ntnu/idatt2105/placeholder/dto/user/RegisterDTO.java
+++ src/main/java/edu/ntnu/idatt2105/placeholder/dto/user/RegisterDTO.java
@@ -1,5 +1,6 @@
 package edu.ntnu.idatt2105.placeholder.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,17 +20,22 @@ import lombok.NonNull;
 public class RegisterDTO {
 
   @NonNull
+  @NotBlank
   private String username;
 
   @NonNull
+  @NotBlank
   private String email;
 
   @NonNull
+  @NotBlank
   private String firstName;
 
   @NonNull
+  @NotBlank
   private String lastName;
 
   @NonNull
+  @NotBlank
   private String password;
 }
