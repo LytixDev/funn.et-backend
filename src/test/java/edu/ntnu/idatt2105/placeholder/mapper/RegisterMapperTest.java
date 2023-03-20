043ec9880700f5a4ad4a215dc9d685dbfdad6978
Carl Gützkow <cjgutzkow@gmail.com>
2023-03-20 19:40:58 +0100

    Create register mapper test

---
+++ src/test/java/edu/ntnu/idatt2105/placeholder/mapper/RegisterMapperTest.java
@@ -0,0 +1,35 @@
+package edu.ntnu.idatt2105.placeholder.mapper;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Nested;
+
+import edu.ntnu.idatt2105.placeholder.dto.user.RegisterDTO;
+import edu.ntnu.idatt2105.placeholder.mapper.user.RegisterMapper;
+import edu.ntnu.idatt2105.placeholder.model.user.User;
+
+public class RegisterMapperTest {
+    @Nested
+    public class RegisterDTOToUserTest {
+  
+      @Test
+      public void testUserToUserDTOConvertsToRoleUSER() {
+        RegisterDTO registration = RegisterDTO.builder()
+            .username("username")
+            .email("email@example.com")
+            .firstName("firstName")
+            .lastName("lastName")
+            .password("password")
+            .build();
+  
+        User user = RegisterMapper.INSTANCE.registerDTOtoUser(registration);
+  
+        assertEquals(registration.getUsername(), user.getUsername());
+        assertEquals(registration.getEmail(), user.getEmail());
+        assertEquals(registration.getFirstName(), user.getFirstName());
+        assertEquals(registration.getLastName(), user.getLastName());
+        assertEquals("USER", user.getRole());
+      }
+    }
+}
