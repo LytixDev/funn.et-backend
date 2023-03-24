e98dbc456706e0f5f604e689f266de9700fa6a6e
Callum Gran <callum.gran@gmail.com>
2023-03-24 14:12:39 +0100

    chats kinda work

--- src/main/java/edu/ntnu/idatt2105/funn/dto/user/ChatDTO.java
+++ src/main/java/edu/ntnu/idatt2105/funn/dto/user/ChatDTO.java
@@ -1,32 +1,36 @@
 package edu.ntnu.idatt2105.funn.dto.user;
 
-import edu.ntnu.idatt2105.funn.model.user.Chat;
 import jakarta.validation.constraints.NotBlank;
-import java.sql.Timestamp;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
-/**
- * Class representing a data transfer object for a message.
- * @author Callum G.
- * @version 1.0 - 23.03.2023
- */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class MessageDTO {
+public class ChatDTO {
 
+  @NonNull
+  @NotBlank
   private Long id;
 
   @NonNull
   @NotBlank
-  private String message;
+  private UserDTO messager;
 
-  private String username;
+  @NonNull
+  @NotBlank
+  private UserDTO listingUser;
 
-  private Timestamp timestamp;
+  @NonNull
+  @NotBlank
+  private Long listingId;
+
+  @NonNull
+  @NotBlank
+  private List<MessageDTO> messages;
 }
