f00522c39354a5f32a9798d97edf0bf80b471a53
Thomas <thomassvendal@gmail.com>
2023-03-26 21:20:58 +0200

    Added sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into category (category_name) values ('Electronics');
+insert into category (category_name) values ('Books');
+insert into category (category_name) values ('Clothing');
+insert into category (category_name) values ('Toys');
+insert into category (category_name) values ('Furniture');
+insert into category (category_name) values ('Sports');
+insert into category (category_name) values ('Tools');
+
